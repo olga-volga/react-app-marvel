1c7842dc3948c5141ea8eb4f3a7ac8492ef5ebb9
olga <yaolmi@yandex.ru>
2022-02-25 21:13:29 +0700

    add React Router to project

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 import {useState} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
 
 import decoration from '../../resources/img/vision.png';
@@ -15,20 +17,29 @@ const App = () => {
         setId(id);
     }
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/*<RandomChar/>
-                <div className="char__content">
-                    <CharList updateId={updateId} />
-                    <ErrorBoundary>
-                        <CharInfo id={id} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>*/}
-                <ComicsList />
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <RandomChar/>
+                            <div className="char__content">
+                                <CharList updateId={updateId} />
+                                <ErrorBoundary>
+                                    <CharInfo id={id} />
+                                </ErrorBoundary>
+                            </div>
+                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <AppBanner />
+                            <ComicsList />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
