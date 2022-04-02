5647243c45184eb81bb19bc8b8ef9f65246b4cde
olga <yaolmi@yandex.ru>
2022-04-02 17:14:59 +0700

    edit character search form

--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Formik, Form, Field, ErrorMessage as FormikErrorMessage } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from  'yup';
 
 import useMarvelService from '../../services/MarvelService';
@@ -11,14 +11,23 @@ import '../../style/button.scss';
 const CharSearchForm = () => {
     const [char, setChar] = useState(null);
     const {loading, error, getCharacterByName, clearError} = useMarvelService();
+    
+    const formik = useFormik({
+        initialValues: {name: ''},
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required('This field is required')
+        }),
+        onSubmit: ({name}) => updateChar(name)
+    });
 
     const updateChar = (name) => {
         clearError();
         getCharacterByName(name)
             .then(res => setChar(res));
-    }
+    };
     const errorMessage = error ? <ErrorMessage /> : null;
-    const message = !char ? null : char.length > 0 ?
+    const message = (!char) ? null : char.length > 0 ?
                     <div className="form__message-block">
                         <div className="form__message form__message_success">There is! Visit {char[0].name} page?</div>
                         <a href="#" className="button button__secondary">
@@ -31,23 +40,24 @@ const CharSearchForm = () => {
     return (
         <div className="search-form">
             <div className="search-form__title">Or find a character by name:</div>
-            <Formik 
-                initialValues={{name: ''}} 
-                validationSchema={Yup.object({
-                    name: Yup.string()
-                        .required('This field is required')
-                })} 
-                onSubmit = {({name}) => {
-                    updateChar(name);
-                }}>
-                    <Form className="form">
-                        <Field id="name" name="name" type="text" placeholder="Enter name" className="form__input" />
-                        <button type="submit" disabled={loading} className="button button__main">
-                            <div className="inner">FIND</div>
-                        </button>
-                        <FormikErrorMessage className="form__message form__message_error" name="name" component="div"/>
-                    </Form>
-            </Formik>
+            <form onSubmit={formik.handleSubmit} className="form">
+                <input 
+                    id="name" 
+                    name="name" 
+                    type="text" 
+                    placeholder="Enter name" 
+                    value={formik.values.name}
+                    onChange={(e) => {
+                        formik.handleChange(e);
+                        setChar(null);
+                    }} 
+                    onBlur={formik.handleBlur}
+                    className="form__input" />
+                <button type="submit" disabled={loading} className="button button__main">
+                    <div className="inner">FIND</div>
+                </button>
+                {formik.errors.name && formik.touched.name ? <div className="form__message form__message_error">{formik.errors.name}</div> : null}
+            </form>
             {errorMessage}
             {message}
         </div>
