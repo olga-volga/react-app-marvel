6c5732cdf3cce1c745c2c33d1932bd19511d1ae5
olga <yaolmi@yandex.ru>
2022-04-17 18:54:12 +0700

    optimize createContent function

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,16 +12,12 @@ const createContent = (process, Component, loadingMoreItems) => {
     switch(process) {
         case 'waiting':
             return <Spinner />;
-            break;
         case 'loading':
             return loadingMoreItems ? <Component /> : <Spinner />;
-            break;
         case 'confirmed':
             return <Component />;
-            break;
         case 'error':
             return <ErrorMessage />;
-            break;
         default:
             throw new Error('Unexpected process state');
     }
--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -10,16 +10,12 @@ const createContent = (process, Component, loadMoreComics) => {
     switch(process) {
         case 'waiting':
             return <Spinner />;
-            break;
         case 'loading':
             return loadMoreComics ? <Component /> : <Spinner />;
-            break;
         case 'confirmed':
             return <Component />;
-            break;
         case 'error':
             return <ErrorMessage />;
-            break;
         default:
             throw new Error('Unexpected process state');
     }
